9ba63776c835171bb3062beea4e33106c629f94d
Pavan <pavanprashanth112@gmail.com>
2024-11-27 12:31:45 +0530

    added back logging feature

--- src/main/java/com/madara/security/security/config/UserDetailsServiceImpl.java
+++ src/main/java/com/madara/security/security/config/UserDetailsServiceImpl.java
@@ -3,6 +3,8 @@ package com.madara.security.security.config;
 import com.madara.security.Exception.UserNotFoundException;
 import com.madara.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,10 +15,14 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username)
-                .orElseThrow(() -> new UserNotFoundException("User not found"));
+                .orElseThrow(() -> {
+                    logger.error("The user {} is not found", username);
+                    return new UserNotFoundException("User not found");
+                });
     }
 }
